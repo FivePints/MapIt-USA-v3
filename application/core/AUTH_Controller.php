0f8d5d58c9d205c1dc93f62a332df47bef219a39
Mike DeVita <mike@gorelative.com>
2012-06-24 03:59:21 -0700

    updated documentation for the core auth controller part of #4

--- application/core/AUTH_Controller.php
+++ application/core/AUTH_Controller.php
@@ -1,6 +1,11 @@
 <?php 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * This is the Core Controller that sets up
+ * all of the default configs and code for 
+ * the authentication area of the app.
+ */
 class AUTH_Controller extends MY_Controller {
 	public function __construct(){
 		parent::__construct();
